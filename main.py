15218552a4791c205f5bdf1a98b0d90bcd5f4e90
P1res2 <pires17.gg@gmail.com>
2025-06-04 23:09:02 -0300

    Atualizando

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-from fastapi import FastAPI, Query
 import requests
+import json
+from fastapi import FastAPI, Query
+from time import perf_counter
 
 app = FastAPI()
 
+JSON_100K = '1zOweCB2jidgHwirp_8oBnFyDgJKkWdDA'
+JSON_1K = '1BX03cWxkvB_MbZN8_vtTJBDGiCufyO92'
+
 file_id = '1zOweCB2jidgHwirp_8oBnFyDgJKkWdDA'
-url = f'https://drive.google.com/uc?export=download&id={file_id}'
-response = requests.get(url=url)
-dados_json = response.json()
+url = f'https://drive.google.com/uc?export=download&id={JSON_1K}'
+
+def request_url():
+    response = requests.get(url=url)
+    return response
+
+@app.get('/evaluation/')
+def evaluation():
+    start_time_response = perf_counter()
+    
+    response = request_url()
+    dados_json = response.json()
+    team_insights(team="UX Wizards")
+    
+    end_time_response = perf_counter()
+    
+    time_response = (end_time_response - start_time_response) * 1000
+    return {
+        "status":response.status_code,
+        "miliseconds":f'{time_response:.2f}ms'
+    }
 
 @app.get('/users/pais/')
 def users(pais: str = Query(None)):
+    response = request_url()
+    dados_json = response.json()
+    
     dados_filtrados = []
     
     for item in dados_json:
@@ -27,6 +53,9 @@ def users(pais: str = Query(None)):
     
 @app.get('/superusers/')
 def super_users():
+    response = request_url()
+    dados_json = response.json()
+    
     dados_filtrados = []
     
     for item in dados_json:
@@ -37,6 +66,7 @@ def super_users():
                 "país":item["country"],
                 "score":item["score"],
                 "ativo":item["active"],
+                
             })
     
     if response.status_code == 200:
@@ -46,11 +76,22 @@ def super_users():
 
 @app.get(f'/team-insights/')
 def team_insights(team: str = Query(None)):
+    response = request_url()
+    dados_json = response.json()
+    
     pessoas_times = []
+    
     total_users = 0
+    active_users = 0
+    leaders = 0
+    completed_projects = 0
+    
     for item in dados_json:
         if item["team"]["name"] == team:
             team_name = item["team"]["name"]
+            
+            if item["active"] == True:
+                active_users += 1
             
             pessoas_times.append({
                 "team_name":item["team"]["name"],
@@ -58,9 +99,8 @@ def team_insights(team: str = Query(None)):
                 "projects":item["team"]["projects"]
             })
             total_users += 1
-            
-    leaders = 0
-    completed_projects = 0
+    active_users_percentage = round((active_users / total_users) * 100, 2)
+    
     for pessoa in pessoas_times:
         if pessoa["leader"] == True:
             leaders += 1
@@ -73,4 +113,5 @@ def team_insights(team: str = Query(None)):
         "people":total_users,
         "leaders":leaders,
         "completed_projects":completed_projects,
+        "active_users_percentage":active_users_percentage,
     }
